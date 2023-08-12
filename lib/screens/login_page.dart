8255db948e202fa33b99040b3478ccb900bd1f18
Vishal Mishra <mishravishal150@gmail.com>
2023-08-12 20:57:17 +0530

    commit 4

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:cataloug_app/utility/routes.dart';
 import 'package:flutter/material.dart';
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+//underscore lagane se public se private me ban jata h koi bhi class 
+
+class _LoginPageState extends State<LoginPage> {
+  String name="";
+  bool changeButton=false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,7 +27,7 @@ class LoginPage extends StatelessWidget {
             height: 20.0,
             ),
           Text(
-            "Welcome",
+            "Welcome $name",
             style: TextStyle(
               fontSize: 28,
               fontWeight: FontWeight.bold,
@@ -39,10 +47,13 @@ class LoginPage extends StatelessWidget {
                   hintText:"Enter Username",
                   
                   labelText: "Username",
-
-
-
-                ), 
+                ),
+                onChanged: (value){
+                  name=value;
+                  setState(() {
+                    
+                  });
+                }, 
                 ),
                 SizedBox(
             height: 20.0,
@@ -59,17 +70,50 @@ class LoginPage extends StatelessWidget {
             SizedBox(
             height: 40.0,
             ),
+//To make any widget as clickable we use to wrap it with "Jester"(no feedback) or "inkwell"(more effective splash colors)
+            InkWell(
+              onTap: () async{
+                setState(() {
+                  changeButton=true; 
+                });
+                //to wait for icon
+                await Future.delayed(Duration(seconds: 1));
+                //Navigator.pushNamed(context, MyRoutes.homeRoute);
+                
+              },
+              //for animation make container to animation container
+              child: AnimatedContainer(
+                duration:Duration(seconds: 1),
+                width: changeButton ? 60 : 150,
+                height: 40,
+                //color: Colors.deepPurple,
+                alignment: Alignment.center,
+                child:changeButton?Icon(Icons.done,color:Colors.white,)
+                :Text(
+                  "Login",
+                  style:TextStyle(
+                    color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),
+                ),
+                decoration: BoxDecoration(
+                  color:Colors.deepPurple,
+                 // shape: changeButton?BoxShape.circle:BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(changeButton ? 50:8)
+                ),
+              ),
+            )
+
+
             //Login Button
-            ElevatedButton(onPressed: (){
-              //print("You are Logged in Successfully");
-              Navigator.pushNamed(context, MyRoutes.homeRoute);//jab login pe click karenge to home pe aa jayenge
-            }, 
-            child: Text("Login"),
-            style: TextButton.styleFrom(
-              minimumSize: Size(150, 40)
+            // ElevatedButton(onPressed: (){
+            //   //print("You are Logged in Successfully");
+            //   Navigator.pushNamed(context, MyRoutes.homeRoute);//jab login pe click karenge to home pe aa jayenge
+            // }, 
+            // child: Text("Login"),
+            // style: TextButton.styleFrom(
+            //   minimumSize: Size(150, 40)
 
               
-            ),)
+            // ),)
               ],
              ),
            )
